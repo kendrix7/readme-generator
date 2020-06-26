b06d28c0fd0d93762f709fc4de276b66c82f0c7a
John Nielsen <johnhayesnielsen@gmail.com>
2020-06-26 00:28:18 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,67 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+// array of questions for user
+const questions = () => {
+    inquirer.prompt([
+     {
+         type: "input",
+         name: "title",
+         message: "What is the title of the project?"
+     },
+     {
+       type: "input",
+       name: "description",
+       message: "What is the description of the project?"
+     },
+     {
+       type: "input",
+       name: "installation",
+       message: "How does someone install the project?"
+     },
+     {
+       type: "input",
+       name: "usage",
+       message: "How does a user use the project?"
+     },
+     {
+       type: "input",
+       name: "authors",
+       message: "Who contributed to the project and are there any acknowledgments?"
+     },
+
+     {
+       type: "input",
+       name: "license",
+       message: "What is the license for the project?"
+     },
+     {
+       type: "input",
+       name: "contributing",
+       message: "Are you open to contributions and what are the requirements for accepting them?"
+     },
+     {
+       type: "input",
+       name: "tests",
+       message: "What are the instructions for running the tests for this project?"
+     },
+     {
+       type: "input",
+       name: "name",
+       message: "What is your full name?"
+     },
+     {
+       type: "input",
+       name: "linkedin",
+       message: "What is your LinkedIn profile name?"
+     },
+     {
+       type: "input",
+       name: "username",
+       message: "What is your GitHub username",  
+     }
+   ])
+
 
 // function to write README file
 function writeToFile(fileName, data) {
